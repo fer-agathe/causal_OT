caaea6433b4947de1a84bdd6682df1ca8d3dc0be
Ewen Gallic <ewen.gallic@gmail.com>
2026-02-20 17:09:08 +0100

    update replication

--- replication/R/transp-categ-objectives.R
+++
@@ -1,5 +0,0 @@
-col_group <- c("#00A08A","#F2AD00", "#1b95e0")
-col_categ <- c("#56B4E9", "#D55E00", "#CC79A7")
-colA <- col_categ[1] ; colB <- col_categ[2] ; colC <- col_categ[3]
-colGpe1 <- col_group[2]
-colGpe0 <- col_group[1]
